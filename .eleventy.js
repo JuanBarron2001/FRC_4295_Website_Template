fa8aa7885bf5d7beabfd61f86c875fa519ae1dbc
JuanBarron2001 <barronjuan2001@gmail.com>
2025-10-06 22:13:24 -0500

    adding cname to the build

--- .eleventy.js
+++ .eleventy.js
@@ -2,5 +2,6 @@ module.exports = function(eleventyConfig) {
     // Copy the entire css folder to _site
     eleventyConfig.addPassthroughCopy("css");
     eleventyConfig.addPassthroughCopy("images");
+    eleventyConfig.addPassthroughCopy("CNAME");
   };
   
