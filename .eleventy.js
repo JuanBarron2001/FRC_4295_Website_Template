aca71181bd1b100dee98b3f5d1b0b24775301f31
JuanBarron2001 <barronjuan2001@gmail.com>
2025-11-18 00:40:29 -0600

    being silly goofy

--- .eleventy.js
+++ .eleventy.js
@@ -3,12 +3,19 @@ require("dotenv").config();
 module.exports = function(eleventyConfig) {
     eleventyConfig.addGlobalData("lambda_url", process.env.LAMBDA_URL);
 
-    // Copy the entire css folder to _site
+    // Pass through copy for static assets
     eleventyConfig.addPassthroughCopy("css");
     eleventyConfig.addPassthroughCopy("js");
     eleventyConfig.addPassthroughCopy("images");
+    eleventyConfig.addPassthroughCopy("favicon");
     eleventyConfig.addPassthroughCopy("CNAME");
     eleventyConfig.addPassthroughCopy("favicon.ico");
     eleventyConfig.addPassthroughCopy("site.webmanifest");
     eleventyConfig.addPassthroughCopy("node_modules/lite-youtube-embed/src");
+    
+    return {
+      dir: {
+        output: "_site"
+      }
+    };
   };
